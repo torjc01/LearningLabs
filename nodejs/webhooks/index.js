68127388a3a15ac408047a2ec7657b765b324b26
torjc01 <julio_cesar.torres@sct.gouv.qc.ca>
2021-05-23 19:45:47 -0400

    webhooks initial commit

---
+++ nodejs/webhooks/index.js
@@ -0,0 +1,17 @@
+// Requires
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+
+// Initializations
+const app = express(); 
+const PORT = 3000; 
+
+// Use body-parser for json parsing 
+app.use(bodyParser.json()); 
+
+app.post("/hook", (req, res) => {
+    console.log(req.body); 
+    res.status(200).end();
+}); 
+// Start express server at the defined port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
